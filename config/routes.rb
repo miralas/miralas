c007e68d3dab41158e55e48e9486dffb0c639f61
Vladislav Petrov <electronicchest@gmail.com>
2013-08-19 15:48:46 +0400

    Configure home to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Miralas::Application.routes.draw do
+  root "home#index", as: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
