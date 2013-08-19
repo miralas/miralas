caad333f729d4810312de6d9982ba1a5d166e054
Vladislav Petrov <electronicchest@gmail.com>
2013-08-19 15:30:04 +0400

    Create project
    rais new miralas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miralas::Application.config.secret_key_base = '292f00c0dc4e2ec20abf634945c2374383d1be9a4d3a652739368fa8ff5069dcd57e0a4ccd1b5c07f8bdbc7ca94a517e47f21fc558d2ed3c66d327472482b24b'
